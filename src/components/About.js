ff09b5f2c4244a8552d00554111d4287443f53c0
Sean Kruse <seanfkruse@gmail.com>
2023-07-17 21:31:08 -0600

    Updated About and public

--- src/components/About.js
+++ src/components/About.js
@@ -1,30 +1,60 @@
 import React from 'react';
-import TeamMembers from 'TeamMembers';
+import { teamMembers } from '../data';
+
+const TeamMembers = ({ members }) => {
+  return (
+    <div className="container mx-auto flex px-10 py-20 md:flex-row flex-col items-center">
+      <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center"></div>
+      {members.map((member) => (
+        <div key={member.name} className="flex flex-col items-center mx-8">
+          <a href={member.profileLink}>
+            <div className="w-80 h-96 border-2 border-gray-300 rounded-lg overflow-hidden mb-4 hover:scale-110 cursor-pointer">
+              <div className="w-full h-4/5 overflow-hidden">
+                <img
+                  className="w-full h-full object-cover object-center"
+                  src={member.profilePicture}
+                  alt={member.name}
+                />
+              </div>
+              <figcaption className="text-center h-1/5 py-2">{member.name}
+              <p className="text-gray-500">{member.role}</p>
+              </figcaption>
+            </div>
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 const About = () => {
   const teamMembers = [
     {
-      name: 'John Doe',
-      role: 'Frontend Developer',
-      profilePicture: './john-doe.jpg',
-    },
-    {
-      name: 'Jane Smith',
-      role: 'Backend Developer',
-      profilePicture: './jane-smith.jpg',
-    },
-    {
-      name: 'Bob Johnson',
+      name: 'Sean Kruse',
       role: 'UI/UX Designer',
-      profilePicture: './bob-johnson.jpg',
+      profilePicture: './animesean.jpg',
+    },
+    {
+      name: 'Chris Lopez',
+      role: 'Backend Engineer',
+      profilePicture: './animechris.jpg',
+    },
+    {
+      name: 'Connor Meek',
+      role: 'API & Data Engineer',
+      profilePicture: './animeconnor.jpg',
     },
   ];
 
   return (
     <section className="text-gray-600 body-font">
-      <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-        <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 mb-10 md:mb-0">
-          <TeamMembers members={teamMembers} />
+      <div className="container mx-auto px-20 py-24">
+        <div className="flex flex-wrap -m-4">
+          <div className="lg:w-1/3 sm:w-1/2 p-4">
+            <div className="flex relative">
+              <TeamMembers members={teamMembers} />
+            </div>
+          </div>
         </div>
       </div>
     </section>
---
+++ src/data.js
@@ -0,0 +1,69 @@
+export const teamMembers = [
+  {
+    title: "JavaScript, HTML, CSS",
+    subtitle: "Note Ninja",
+    description:
+      "The ultimate Chrome extension for seamless note-taking and organization. With Note Ninja, you can effortlessly save important information to Microsoft Word or Google Docs while browsing the web. ",
+    image: "./project-1.gif",
+  },
+  {
+    title: "Javascript, HTML, CSS",
+    subtitle: "Microsoft Outlook Signature Extension",
+    description:
+      "An extension that allows users to create and manage their email signatures in Microsoft Outlook. Account management is handled directly through the extension.",
+    image: "./project-2.gif",
+    link: "https://github.com/SeanKruse/school-projects/tree/main/Software%20Development%20Method%20and%20Tools",
+  },
+  {
+    title: "Javascript, HTML, CSS",
+    subtitle: "Microsoft Outlook Signature Extension",
+    description:
+      "An extension that allows users to create and manage their email signatures in Microsoft Outlook. Account management is handled directly through the extension.",
+    image: "./project-2.gif",
+  },
+];
+
+export const projects = [
+    {
+      title: "JavaScript, HTML, CSS",
+      subtitle: "Note Ninja",
+      description:
+        "The ultimate Chrome extension for seamless note-taking and organization. With Note Ninja, you can effortlessly save important information to Microsoft Word or Google Docs while browsing the web. ",
+      image: "./project-1.gif",
+    },
+    {
+      title: "Javascript, HTML, CSS",
+      subtitle: "Microsoft Outlook Signature Extension",
+      description:
+        "An extension that allows users to create and manage their email signatures in Microsoft Outlook. Account management is handled directly through the extension.",
+      image: "./project-2.gif",
+      link: "https://github.com/SeanKruse/school-projects/tree/main/Software%20Development%20Method%20and%20Tools",
+    },
+  ];
+  
+  export const testimonials = [
+    {
+      quote:
+        "Note Ninja is a beneficial tool for students who use Chrome for their learning purpose. The feature to export this to a Word document makes it easy to organize the notes.",
+      image: "./capture.png",
+      name: "Ranjidha Anil",
+      company: "MSU Denver",
+    },
+    {
+      quote:
+        "As an educator, I think Note Ninja is a great tool for students and researchers. It is very user-friendly and allows export notes to Google Docs and Microsoft Word.",
+      image: "./unnamed.jpg",
+      name: "Thyago Mota",
+      company: "MSU Denver",
+    },
+  ];
+  
+  export const skills = [
+    "JavaScript",
+    "React",
+    "Python",
+    "Node",
+    "Java",
+    "GIT",
+  ];
+  
